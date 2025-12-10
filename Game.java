471fa5bd7f8070f456eac347c6a5961a7f2af60a
yzrakei <156145194+yzrakei@users.noreply.github.com>
2025-12-10 17:40:29 -0500

    Temporary Window Test

--- Game.java
+++ Game.java
@@ -1,14 +1,18 @@
 import javax.swing.JFrame;
+import java.awt.Color;
 
 public class Game {
   public static void main(String[] args) {
     //run main game here
     JFrame window = new JFrame();
+    window.setSize(800, 720);
     window.setResizable(false);
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     window.setTitle("SHALL YOU PASS JAVA?");
+    window.getContentPane().setBackground(Color.BLACK);
 
-    window.pack();
+    //window.pack(); --> when adding panels
+
     window.setVisible(true);
   }
 }
--- Player.java
+++ Player.java
@@ -2,7 +2,7 @@ public class Player {
   private int health;
   private int STARTING_HEALTH = 3;
 
-  public player() {
+  public Player() {
     this.health = STARTING_HEALTH;
     
   }
