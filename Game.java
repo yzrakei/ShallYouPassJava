260e41a10a84c3ad894309ec9ad039b9985b2bfd
yzrakei <156145194+yzrakei@users.noreply.github.com>
2025-12-10 16:49:32 -0500

    testing window
    Added a JFrame to create a game window with title.

--- Game.java
+++ Game.java
@@ -1,5 +1,14 @@
+import javax.swing.JFrame;
+
 public class Game {
   public static void main(String[] args) {
     //run main game here
+    JFrame window = new JFrame();
+    window.setResizable(false);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setTitle("SHALL YOU PASS JAVA?");
+
+    window.pack();
+    window.setVisible(true);
   }
 }
